bbd9af486e2132693d0c3b0c084dc98ccbfe2845
Arpita Roy <arpita.roy.workar@gmail.com>
2023-11-06 19:42:29 +0530

    wip

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
 import { propertyForSaleArr } from "/properties/propertyForSaleArr.js";
 import { placeholderPropertyObj } from "/properties/placeholderPropertyObj.js";
-console.log(placeholderPropertyObj);
-/*
-SUPER CHALLENGE 💪
 
-Render out a card for each of the properties in the propertyForSaleArr array (in the 'properties' folder). Each card should have an image, a property location, a price, a comment and the TOTAL property size in square metres (each object has an array with the size in square metres of the individual rooms).
-
-If no array of properties is passed to getPropertyHtml, the placeholder property stored in placeholderPropertyObj (in the 'properties' folder) should be rendered instead.
-
-This is the JS I want you to use to complete this challenge 👇
-- import/export
-- .map()
-- .join()
-- Object destructuring
-- .reduce()
-- Default parameters
-
-*/
-function getPropertyHtml(defaultProperty = "", property) {
+function getPropertyHtml(property = [placeholderPropertyObj]) {
   const renderHTML = property
     .map((item) => {
       const eachRoomM2 = item.roomsM2.reduce(
         (total, currentItem) => total + currentItem
       );
+      const { image, propertyLocation, priceGBP, comment } = item;
       return `
             <section class="card">
-                <img src="/images/${item.image}">
+                <img src="/images/${image}">
                 <div class="card-right">
-                    <h2>${item.propertyLocation}</h2>
-                    <h3>$${item.priceGBP}</h3>
-                    <p>${item.comment}</p>
+                    <h2>${propertyLocation}</h2>
+                    <h3>$${priceGBP}</h3>
+                    <p>${comment}</p>
                     <h3>${eachRoomM2} m2</h3>
                 </div>
             </section> 
@@ -40,6 +25,4 @@ function getPropertyHtml(defaultProperty = "", property) {
 }
 
 /***** Modify 👇 by adding an argument to the function call ONLY. *****/
-document.getElementById("container").innerHTML = getPropertyHtml(
-  placeholderPropertyObj
-);
+document.getElementById("container").innerHTML = getPropertyHtml();
